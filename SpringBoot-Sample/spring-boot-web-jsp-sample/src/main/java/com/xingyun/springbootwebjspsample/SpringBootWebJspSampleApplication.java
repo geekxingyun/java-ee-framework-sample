646c353651390b7a37423930c02b4978077c1451
geekxingyun <fairy_xingyun@hotmail.com>
2018-04-06 10:39:18 +0800

    move source code

---
+++ SpringBoot-Sample/spring-boot-web-jsp-sample/src/main/java/com/xingyun/springbootwebjspsample/SpringBootWebJspSampleApplication.java
@@ -0,0 +1,12 @@
+package com.xingyun.springbootwebjspsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebJspSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebJspSampleApplication.class, args);
+	}
+}
---
+++ SpringBoot-Sample/spring-boot-web-jsp-sample/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: fairy
+  Date: 4/6/2018
+  Time: 9:56 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+  Hello World,Spring Boot 2.0 JSP Sample
+</body>
+</html>
