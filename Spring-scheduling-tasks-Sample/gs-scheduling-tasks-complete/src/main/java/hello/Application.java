d0e7f179dcb0eb37a84ce905a5757a65fbfed484
geekxingyun <fairy_xingyun@hotmail.com>
2018-05-29 19:47:26 +0800

    added scheduling-tasks sample

---
+++ Spring-scheduling-tasks-Sample/gs-scheduling-tasks-complete/src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class);
+    }
+}
