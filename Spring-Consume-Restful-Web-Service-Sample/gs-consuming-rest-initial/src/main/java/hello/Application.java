98e25d7250ed9899bf8626ef97cdd2eec6fcd525
geekxingyun <fairy_xingyun@hotmail.com>
2018-05-29 12:25:19 +0800

    added consume a restful web service

---
+++ Spring-Consume-Restful-Web-Service-Sample/gs-consuming-rest-initial/src/main/java/hello/Application.java
@@ -0,0 +1,40 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Application.class);
+		
+//		 RestTemplate restTemplate = new RestTemplate();
+//	     Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+//	     log.info(quote.toString());
+
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
+
+}
