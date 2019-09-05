54d8ac0cc241598eea99e76cbaa2fcd7f48990a2
fairy <fairy_xingyun@hotmail.com>
2019-09-06 07:10:33 +0800

    added CodeFactory Sample

---
+++ JavaDesignPatternSample/FactoryPatternSample/CodeFactoryPatternSample/src/main/java/com/xingyun/interfaces/IUser.java
@@ -0,0 +1,16 @@
+package com.xingyun.interfaces;
+
+import com.xingyun.model.User;
+
+/**
+ * @author 星云
+ * @Description 用户服务接口层
+ * @Date 9/5/2019 8:09 AM
+ */
+public interface IUser {
+	/**
+	 * Insert User Interface
+	 * @param user
+	 */
+	void insertUser(User user);
+}
