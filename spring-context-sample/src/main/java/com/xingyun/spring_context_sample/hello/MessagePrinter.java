8fca588c10d6322f9327b3644df3cda67d314180
fairy <fairy_xingyun@hotmail.com>
2019-09-07 14:45:30 +0800

    added codes

---
+++ spring-context-sample/src/main/java/com/xingyun/spring_context_sample/hello/MessagePrinter.java
@@ -0,0 +1,18 @@
+package com.xingyun.spring_context_sample.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessagePrinter {
+	final private MessageService service;
+
+    @Autowired
+    public MessagePrinter(MessageService service) {
+        this.service = service;
+    }
+
+    public void printMessage() {
+        System.out.println(this.service.getMessage());
+    }
+}
---
+++ spring-context-sample/src/main/java/com/xingyun/spring_context_sample/hello/MessageService.java
@@ -0,0 +1,5 @@
+package com.xingyun.spring_context_sample.hello;
+
+public interface MessageService {
+	String getMessage();
+}
